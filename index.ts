d218ed4b3b865f20f46eab2e7eac3c41cecd7eea
aeovac <predam@gmx.com>
2025-02-20 19:56:49 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,51 +1,46 @@
-import '@tensorflow/tfjs';
+//import '@tensorflow/tfjs';
 import * as Model from '@tensorflow-models/toxicity';
-import * as Discord from 'discord.js';
+import * as Discord from 'lilybird';
 
 const indice = 0.9; //Sensibility
-const messages: Discord.Message[] = []; 
+const messages: Discord.Message.Structure[] = []; 
 const interval = 5_000; // Batch interval
 const discord_token = ''; //Your bot token, https://discord.com/developers/applications
 
-const client = new Discord.Client({ intents: 12 });
 const classifier = await Model.load(indice, ['toxicity']);
+const client = new Discord.Client({ 
+    intents: 131071,
+    listeners: {
+        messageCreate(_, message) {
+            let bypass = false;
 
-client.once('ready', () => {
-    console.log('Logged in');
-
-    setInterval(async () => {
-        if(messages.length > 0) {
-            const msgs = messages
-                .splice(0, messages.length)
-                .map(({ content }) => (content));
-
-            const results = await classifier.classify(msgs);
-            results.forEach(({ results }) => {
-                if (results.length <= 0) {
-                    return;
-                }
-
-                results.forEach(({ match }, index) => {
-                    const message = messages.at(index);
-
-                    if (match && message) {
-                        message?.delete();
-                    }
-                });
-            });
+            if(!bypass) {
+                messages.push(message);
+            } 
         }
-    }, interval);
-});
+    },
+    async setup(client) {
+        console.log('Logged in');
+        setInterval(async () => {
+            if(messages.length > 0) {
+                const msgs = messages.splice(0, messages.length)
+                const results = await classifier.classify(msgs.map(({ content }) => (content)));
+                results.forEach(({ results }) => {
+                    if (results.length <= 0) {
+                        return;
+                    }
+    
+                    results.forEach(async ({ match }, index) => {
+                        const { channel_id, id } = msgs.at(index)!;
 
-client.on('messageCreate', (message) => {
-    const { author, member, guild } = message;
-    const bypass = author.bot || member?.permissions.has('Administrator') || guild?.ownerId == author.id;
-
-    if(bypass) {
-        return;
+                        if (match && id) {
+                            await client.rest.deleteMessage(channel_id, id);
+                        }
+                    });
+                });
+            }
+        }, interval);
     }
-
-    messages.push(message);
 });
 
 client.login(discord_token);
